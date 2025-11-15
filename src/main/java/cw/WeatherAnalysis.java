e2dd0ff73d751e35252fc73e2d0845159febe9dc
yenuli <yenuliboyagoda@gmail.com>
2025-11-15 11:57:38 +0530

    comit the pom.xml file.

---
+++ src/main/java/cw/WeatherAnalysis.java
@@ -0,0 +1,26 @@
+//import javax.security.auth.login.Configuration;
+
+public class WeatherAnalysis {
+    // public static class MapperTest extends Mapper<
+
+
+
+    // public static void main(String[] args) {
+
+    //     //creates a hadooop configuration object
+    //     Configuration conf = new Configuration();
+
+    //     //args -> what you type in command line after the class name
+    //     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
+    //     if(otherArgs.length != 2){
+    //         System.err.println("Usage: Alayise Weather <in> <out>");
+    //         System.exit(2);
+    //     }
+
+    //     Job job = Job.getInstance(conf , "WeatherAnalysis");
+    //     job.setJarByClass(WeatherAnalysis.class);  //setJarByClass tells Hadoop where to find the code (in which jar).
+
+    // }
+    
+}
